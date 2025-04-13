155c5c4197a735cb45e83e8c84128716ca86fd57
tescoph2025 <tescoph2025@gmail.com>
2025-04-13 18:56:20 +0800

    admin request fund

--- app/Http/Controllers/AdminRequestFund/AdminRequestFundController.php
+++ app/Http/Controllers/AdminRequestFund/AdminRequestFundController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminRequestFund;
 
 use App\Http\Controllers\Controller;
+use App\Models\RequestFund;
 use App\Models\WithdrawTransaction;
 use App\Services\AccountBalanceService;
 use Illuminate\Http\Request;
@@ -33,33 +34,35 @@ class AdminRequestFundController extends Controller
 
         return Inertia::render('admin/admin-approved-requestfund', [
             'data' => $data,
+            'success' => session('success'),
+            'error' => session('error'),
             // 'data' => $data->toArray(),
         ]);
     }
 
     public function pendingReqFund()
     {
-
-        $data = DB::table('withdraw_transaction as a')
-            ->join('users as b', 'a.user_id', '=', 'b.id')
-            ->leftJoin('user_bank_details as c', 'c.user_id', '=', 'a.id')
-            ->select(
-                'a.id',
-                'b.name',
-                'b.email',
-                'a.withdrawal_amount as amount',
-                'c.bank_name',
-                'c.bank_account_name',
-                'c.bank_account_number',
-                'a.created_at'
-            )
-            ->where('a.status', 'pending')
+        $data = DB::table('request_fund')
+            ->select([
+                'request_fund.status',
+                'request_fund.created_at as request_date',
+                'request_fund.amount',
+                'receiving_bank.bank_name',
+                'receiving_bank.account_number',
+                'users.name',
+                'users.email',
+                'request_fund.updated_at as approved_date'
+            ])
+            ->leftJoin('receiving_bank', 'receiving_bank.id', '=', 'request_fund.bank_id')
+            ->leftJoin('users', 'users.id', '=', 'request_fund.user_id')
+            ->where('request_fund.status', '=', 'pending')
+            ->orderByDesc('request_fund.updated_at')
             ->get();
 
-
         return Inertia::render('admin/admin-pending-requestfund', [
-            'data' => $data->toArray(),
-            'success' => session('success')
+            'data' => $data,
+            'success' => session('success'),
+            'error' => session('error'),
         ]);
     }
     public function approveReqFund(Request $request)
@@ -68,11 +71,11 @@ class AdminRequestFundController extends Controller
 
         $accountBalService = new AccountBalanceService();
 
-        $withdraw_trans = WithdrawTransaction::find($request->id);
-        $withdraw_trans->status = 'approved';
-        $withdraw_trans->save();
+        $requestFund = RequestFund::find($request->id);
+        $requestFund->status = 'approved';
+        $requestFund->save();
 
-        $accountBalService->deductAccountBalance($withdraw_trans->user_id, $withdraw_trans->withdrawal_amount);
+        $accountBalService->addAccountBalance($requestFund->user_id, $requestFund->amount);
 
         return redirect()->back()->with('success', ['message' => 'Request approved!', $time]);
     }
@@ -81,9 +84,9 @@ class AdminRequestFundController extends Controller
     {
         $time = now()->format('H:i:s');
 
-        $withdraw_trans = WithdrawTransaction::find($request->id);
-        $withdraw_trans->status = 'denied';
-        $withdraw_trans->save();
+        $requestFund = RequestFund::find($request->id);
+        $requestFund->status = 'denied';
+        $requestFund->save();
 
         return redirect()->back()->with('success', ['message' => 'Request denied!', $time]);
     }
